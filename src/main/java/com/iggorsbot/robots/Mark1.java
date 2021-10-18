7e9682e3843f42746954bafd11dde1a3b90d23df
iggorsbot <iggorsbot@gmail.com>
2021-10-18 21:58:57 +0300

    first commit

---
+++ src/main/java/com/iggorsbot/robots/Mark1.java
@@ -0,0 +1,13 @@
+package com.iggorsbot.robots;
+
+public class Mark1 {
+    int power = 0;
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getPower() {
+        return this.power;
+    }
+}
