3ebfbb13affff0860c144e6ea7266001e7fa231c
iggorsbot <iggorsbot@gmail.com>
2021-10-19 01:36:18 +0300

    add health

--- src/main/java/com/iggorsbot/robots/Mark1.java
+++ src/main/java/com/iggorsbot/robots/Mark1.java
@@ -3,6 +3,7 @@ package com.iggorsbot.robots;
 public class Mark1 {
     int power = 10000;
     int cost = 1000;
+    int health = 1500;
 
     public void setPower(int power) {
         this.power = power;
