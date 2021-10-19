dad7f5a8d5a37e7092d877e7c08fb3868b72ee10
iggorsbot <iggorsbot@gmail.com>
2021-10-19 09:10:37 +0300

    remove

--- src/main/java/com/iggorsbot/robots/Mark1.java
+++ src/main/java/com/iggorsbot/robots/Mark1.java
@@ -1,23 +1,5 @@
 package com.iggorsbot.robots;
 
 public class Mark1 {
-    int power = 10000;
-    int cost = 1000;
-    int health = 1515500;
 
-    public void setPower(int power) {
-        this.power = power;
-    }
-
-    public int getPower() {
-        return this.power;
-    }
-
-    public int getCost() {
-        return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
 }
